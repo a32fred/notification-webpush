811425f72024e96f4c6df19590d508f3d090ff58
a32fred <a.32.freeed@gmail.com>
2023-10-24 03:07:38 -0300

    notfy-with-webpush

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,49 @@
-import { useEffect } from 'react';
+
 const Home = () => {
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(async (serviceWorker) => {
+  if (typeof window !== 'undefined') {
 
-        let subscription = await serviceWorker.pushManager.getSubscription()
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(async (serviceWorker) => {
 
-        if (!subscription) {
-          const response = await fetch('https://socketio.a32fred.repl.co/key');
-          const data = await response.json();
-          subscription = await serviceWorker.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: data.publicKey
-          })
+          let subscription = await serviceWorker.pushManager.getSubscription()
 
-          await fetch('https://socketio.a32fred.repl.co/registerFCMToken', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ subscription }),
-          });
+          if (!subscription) {
+            const response = await fetch('https://socketio.a32fred.repl.co/key');
+            const data = await response.json();
+            subscription = await serviceWorker.pushManager.subscribe({
+              userVisibleOnly: true,
+              applicationServerKey: data.publicKey
+            })
 
-          await fetch('https://socketio.a32fred.repl.co/sendNotification', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ subscription }),
-          });
+            await fetch('https://socketio.a32fred.repl.co/registerFCMToken', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ subscription }),
+            });
 
-        }
+            await fetch('https://socketio.a32fred.repl.co/sendNotification', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ subscription }),
+            });
+
+          }
 
 
-        console.log('Service Worker registrado com sucesso:');
-      })
-      .catch((error) => {
-        console.error('Erro ao registrar Service Worker:', error);
-      });
+          console.log('Service Worker registrado com sucesso:');
+        })
+        .catch((error) => {
+          console.error('Erro ao registrar Service Worker:', error);
+        });
+    }
   }
+
 
 
   return (
